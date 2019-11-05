e92fdad99d5193eda210e71b6375e2dbffa5f772
kleyson castro <kleysoncastro99@gmail.com>
2019-11-04 22:47:07 -0300

    chore: using json-server API, function created to format BRL value

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,107 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../server/api';
+import { formatPrice } from '../../util/format';
 import { ListProcuct } from './styles';
 
-function Home() {
-  return (
-    <ListProcuct>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-attitude-576/88/D22-3009-088/D22-3009-088_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tênis top de linha</strong>
-        <span>R$ 179.90</span>
+class Home extends Component {
+  state = { products: [] };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR OU CARRINHO</span>
-        </button>
-      </li>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tênis top de linha</strong>
-        <span>R$ 179.90</span>
+    // para formatar os valores para real BR
+    const data = response.data.map(product => ({
+      ...product,
+      preceFormatted: formatPrice(product.price),
+    }));
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR OU CARRINHO</span>
-        </button>
-      </li>
+    this.setState({ products: data });
+  }
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-attitude-576/88/D22-3009-088/D22-3009-088_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tênis top de linha</strong>
-        <span>R$ 179.90</span>
+  render() {
+    const { products } = this.state;
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR OU CARRINHO</span>
-        </button>
-      </li>
+    return (
+      <ListProcuct>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.preceFormatted}</span>
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-band-masculino/93/D22-2024-793/D22-2024-793_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tênis top de linha</strong>
-        <span>R$ 179.90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR OU CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-attitude-576/88/D22-3009-088/D22-3009-088_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tênis top de linha</strong>
-        <span>R$ 179.90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR OU CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-feminino/18/D24-1739-018/D24-1739-018_detalhe2.jpg?ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tênis top de linha</strong>
-        <span>R$ 179.90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR OU CARRINHO</span>
-        </button>
-      </li>
-    </ListProcuct>
-  );
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>ADICIONAR OU CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ListProcuct>
+    );
+  }
 }
 
 export default Home;
