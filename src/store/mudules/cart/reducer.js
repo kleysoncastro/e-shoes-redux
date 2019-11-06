6487118029cf2aec056068f5b3b2634a6b5f4f0e
kleyson castro <kleysoncastro99@gmail.com>
2019-11-05 21:32:32 -0300

    chore: add item to cart

--- src/store/mudules/cart/reducer.js
+++ src/store/mudules/cart/reducer.js
@@ -2,7 +2,13 @@ export default function cart(state = [], action) {
   // switch é para que somente o redure cart ouça eventos dele mesmo;
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      return [
+        ...state,
+        {
+          ...action.product,
+          amount: 1,
+        },
+      ];
     default:
       return state;
   }
